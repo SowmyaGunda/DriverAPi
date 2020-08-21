83f1a4f560cd645c3fb0c34ff3cc0e196151ee6e
SowmyaGunda <sowmya.gunda9@gmail.com>
2020-08-21 17:02:59 +0100

    add project packages layout

---
+++ src/main/java/com/technicaltest/driverapi/DriverapiApplication.java
@@ -0,0 +1,13 @@
+package com.technicaltest.driverapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriverapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriverapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/technicaltest/driverapi/DriverapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technicaltest.driverapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriverapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
